e68c31bbc6447da466c30fb2c9424c82dd979644
lucassherwin <lksherwin@gmail.com>
2020-08-10 19:07:42 -0400

    added search screen

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+const navigator = createStackNavigator({
+  
+});
 
 export default function App() {
   return (
