64513224caf59a58f7bf5215e91ada95e4788dd3
lucassherwin <lksherwin@gmail.com>
2020-08-11 16:32:31 -0400

    hid api key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer 9Zhi3szkheGXxUxVUgzM72Ne3AelI8Wrkzn6A00BqPIRPcz0fZE2R0jkVn6mQKpQebZqfOuFqcsi3tHiTqGDmw-oZ7YEYzeK0eVu_oU3Ezc6ajNXdoA2jaafuMExX3Yx'
+        Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}` 
     }
 });
 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar.js';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState(''); //term is the search term we are passing to the API
+    const [results, setResults] = useState([]);
     return (
         <View>
             <SearchBar 
