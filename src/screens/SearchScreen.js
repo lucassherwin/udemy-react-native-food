ee819beeb6c0b5d1ff04f82b1dc263d5ce4e68c6
lucassherwin <lksherwin@gmail.com>
2020-08-12 17:00:56 -0400

    added results list, api key hidden

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar.js';
 import yelp from '../api/yelp';
 import useResults from '../hooks/useResults.js';
+import ResultsList from '../components/ResultsList.js'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState(''); //term is the search term we are passing to the API
@@ -17,6 +18,9 @@ const SearchScreen = () => {
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
+            <ResultsList title="Cost Effective" />
+            <ResultsList title="Bit Pricier" />
+            <ResultsList title="Big Spender" />
         </View>
     )
 }
