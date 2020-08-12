688ca4b24a42e08715bdd949b43ca42fe8124588
lucassherwin <lksherwin@gmail.com>
2020-08-12 16:53:37 -0400

    refactored hoooks, api key hidden

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
+// var apikey = process.env.REACT_APP_YELP_API_KEY;
 
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}` 
+        // Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}` 
     }
 });
 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,20 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar.js';
 import yelp from '../api/yelp';
+import useResults from '../hooks/useResults.js';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState(''); //term is the search term we are passing to the API
-    const [results, setResults] = useState([]);
+    const [searchApi, results, errorMessage] = useResults();
+
     return (
         <View>
             <SearchBar 
                 term={term}
-                onTermChange={(newTerm) => setTerm(newTerm)}
-                onTermSubmit={() => console.log('term was submitted')}
+                onTermChange={setTerm}
+                onTermSubmit={() => searchApi(term)}
             />
-            <Text>Search Screen</Text>
-            <Text>{term}</Text>
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We have found {results.length} results</Text>
         </View>
     )
 }
